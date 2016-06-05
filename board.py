9ad8657372cd3301837830bbfb39ca967f050650
Gavin Brown <gavinrbrown1@gmail.com>
2016-06-05 19:29:48 -0400

    Start board file

---
+++ board.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# minesweeper board
+
+class Board(self, rows, cols, mines):
+
+    def __init__(self, rows, cols, mines):
+        self.rows = rows
+        self.cols = cols
+        self.mines = mines
+
+        
